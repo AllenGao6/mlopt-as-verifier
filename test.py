c5db1c46a633d3ebdd579775ec9d929104187277
Allen Gao <allengaosh@icloud.com>
2024-04-25 17:13:39 -0700

    a

--- test.py
+++ test.py
@@ -1,65 +1,68 @@
-import matplotlib.pyplot as plt
-from mlopt.sampling import uniform_sphere_sample
-import mlopt
-import pandas as pd
-import cvxpy as cp
 import numpy as np
-import os
+import cvxpy as cp
+import pandas as pd
+import logging
 
-np.random.seed(0)
+import mlopt
+from mlopt.sampling import uniform_sphere_sample
+from mlopt.learners import XGBoost
+from mlopt.utils import n_features, pandas2array
 
-np.random.seed(1)
-T = 30
-M = 3.
-h = 1.
-c = 2.
-p = 3.
+np.random.seed(1)  # Reset random seed for reproducibility
 
-# Define problem
-x = cp.Variable(T+1)
-u = cp.Variable(T)
+# Variable
+n = 10
+x = cp.Variable(n, integer=True)
 
-# Define parameters
-d = cp.Parameter(T, nonneg=True, name="d")
-x_init = cp.Parameter(1, name="x_init")
+# Cost
+c = np.random.rand(n)
 
-# Constaints
-constraints = [x[0] == x_init]
-for t in range(T):
-    constraints += [x[t+1] == x[t] + u[t] - d[t]]
-constraints += [u >= 0, u <= M]
+# Weights
+a = cp.Parameter(n, nonneg=True, name='a')
+x_u = cp.Parameter(n, nonneg=True, name='x_u')
+b = 0.5 * n
 
-# Objective
-cost = cp.sum(cp.maximum(h * x, -p * x)) + c * cp.sum(u)
+# Problem
+cost = - c @ x
+constraints = [a @ x <= b,
+               0 <= x, x <= x_u]
+
 
 # Define optimizer
+# If you just want to remove too many messages
+# change INFO to WARNING
 problem = cp.Problem(cp.Minimize(cost), constraints)
-m = mlopt.Optimizer(problem)
+m = mlopt.Optimizer(problem,
+                    log_level=logging.INFO)
+
 
 # Average request
-theta_bar = np.concatenate(( 2 * np.ones(T),  # d
-                            [10]              # x_init
-                           ))
-radius = 1
+theta_bar = 2 * np.ones(2 * n)
+radius = 1.0
 
 
-def sample_inventory(theta_bar, radius, n=100):
+def sample(theta_bar, radius, n=100):
 
     # Sample points from multivariate ball
-    X_d = uniform_sphere_sample(theta_bar[:-1], radius, n=n)
-    X_x_init = uniform_sphere_sample([theta_bar[-1]], 3 * radius,
-                                     n=n)
+    ndim = int(len(theta_bar)/2)
+    X_a = uniform_sphere_sample(theta_bar[:ndim], radius, n=n)
+    X_u = uniform_sphere_sample(theta_bar[ndim:], radius, n=n)
 
-    df = pd.DataFrame({'d': X_d.tolist(),
-                       'x_init': X_x_init.tolist()})
+    df = pd.DataFrame({
+        'a': list(X_a),
+        'x_u': list(X_u)
+        })
 
     return df
+
 
 # Training and testing data
 n_train = 1000
 n_test = 100
-theta_train = sample_inventory(theta_bar, radius, n=n_train)
-theta_test = sample_inventory(theta_bar, radius, n=n_test)
+theta_train = sample(theta_bar, radius, n=n_train)
+theta_test = sample(theta_bar, radius, n=n_test)
 
-# Train solver
-m.train(theta_train, learner=mlopt.PYTORCH)
+m.train(theta_train, learner=mlopt.XGBOOST)
+
+results = m.performance(theta_test)
+print("Accuracy: %.2f " % results[0]['accuracy'])
