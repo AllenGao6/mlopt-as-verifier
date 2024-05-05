8a438db509525f464734ad4c9001bfde699153f8
Allen Gao <allengaosh@icloud.com>
2024-05-04 20:11:38 -0700

    added Runner info

--- Runner.py
+++ Runner.py
@@ -2,6 +2,7 @@ import numpy as np
 import cvxpy as cp
 import pandas as pd
 import logging
+
 import mlopt
 from mlopt.sampling import uniform_sphere_sample
 from mlopt.learners import XGBoost
@@ -10,7 +11,7 @@ from mlopt.utils import n_features, pandas2array
 np.random.seed(1)  # Reset random seed for reproducibility
 
 n = 5  # Number of neurons
-layer = 5  # Number layers
+layer = 5  # Number of layers
 M = 1e4
 
 x = cp.Variable((n, layer + 1))
@@ -18,41 +19,40 @@ z_1 = cp.Variable((n, layer), boolean=True)
 z_2 = cp.Variable((n, layer), boolean=True)
 z_out = cp.Variable(n, boolean=True)
 x_out = cp.Variable(1)
-
 constr = []
 
-# Parameters for weights and biases
-W = [cp.Parameter((n, n), name=f'W_{l}') for l in range(layer)]
-b = [cp.Parameter(n, name=f'b_{l}') for l in range(layer)]
+# Parameters for weights and biases, reshaping W to 2D
+W = cp.Parameter((n * layer, n), name='W')  # Changed dimension of W
+b = cp.Parameter((n, layer), name='b')
 
 for l in range(layer):
     for j in range(n):
         ''' Encoding ReLu activation function '''
-        constr += [x[j, l + 1] >= W[l][:, j] @ x[:, l] + b[l][j],
+        # Update matrix multiplication to align with the reshaped W
+        W_l = W[l*n:(l+1)*n, :]  # Extract weights for layer l
+        constr += [x[j, l + 1] >= W_l[:, j] @ x[:, l] + b[j, l],
                    x[j, l + 1] >= 0,
-                   x[j, l + 1] <= W[l][:, j] @ x[:, l] + b[l][j] + (1 - z_1[j, l]) * M,
+                   x[j, l + 1] <= W_l[:, j] @ x[:, l] + b[j, l] + (1 - z_1[j, l]) * M,
                    x[j, l + 1] <= (1 - z_2[j, l]) * M,
                    z_1[j, l] + z_2[j, l] == 1]
 
 ''' Specify input range '''
-constr += [x[:, 0] <= np.zeros(n) + 1,
-           x[:, 0] >= np.zeros(n) - 1]
+constr += [x[:, 0] <= np.zeros(n) + 1, x[:, 0] >= np.zeros(n) - 1]
 
 ''' Specify output layer maximum'''
 for j in range(n):
-    constr += [x_out >= x[j, layer],
-               x_out <= x[j, layer] + (1 - z_out[j]) * M]
-
+    constr += [x_out >= x[j, layer], x_out <= x[j, layer] + (1 - z_out[j]) * M]
 constr += [cp.sum(z_out) == 1]
 
 # Construct a CVXPY problem
 objective = cp.Minimize(cp.norm(x_out, 1))
 prob = cp.Problem(objective, constr)
-
 m = mlopt.Optimizer(prob, log_level=logging.INFO)
 
+
+
 # Average request
-theta_bar = 2 * np.ones(n * layer * n + n * layer)  # Adjust dimensions for weights and biases
+theta_bar = 2 * np.ones(n * n * layer + n * layer)
 radius = 1.0
 
 def uniform_sphere_sample(center, radius, n=100):
@@ -61,10 +61,15 @@ def uniform_sphere_sample(center, radius, n=100):
 
 def sample(theta_bar, radius, n=100):
     # Sample points from multivariate ball
-    X_W = [uniform_sphere_sample(theta_bar[l*n*n:(l+1)*n*n], radius, n=n).reshape(n, n, n) for l in range(layer)]
-    X_b = [uniform_sphere_sample(theta_bar[n*n*layer+l*n:n*n*layer+(l+1)*n], radius, n=n).reshape(n, n) for l in range(layer)]
-    df = pd.DataFrame({f'W_{l}': [X_W[l][i] for i in range(n)] for l in range(layer)} | {f'b_{l}': [X_b[l][i] for i in range(n)] for l in range(layer)})
+    X_W = uniform_sphere_sample(theta_bar[:n*n*layer], radius, n=n).reshape(n, layer * n, n)
+    X_b = uniform_sphere_sample(theta_bar[n*n*layer:], radius, n=n).reshape(n, layer, n)
+
+    df = pd.DataFrame({
+        'W': list(X_W),
+        'b': list(X_b)
+    })
     return df
+
 
 # Training and testing data
 n_train = 1000
@@ -73,6 +78,8 @@ theta_train = sample(theta_bar, radius, n=n_train)
 theta_test = sample(theta_bar, radius, n=n_test)
 
 m.train(theta_train, learner=mlopt.XGBOOST)
+
+
 results = m.performance(theta_test)
 print("Accuracy: %.2f " % results[0]['accuracy'])
 
