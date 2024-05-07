2bde5be4db16d072fde94be7889f77ae41aa3385
Allen Gao <allengaosh@icloud.com>
2024-05-07 02:03:18 -0700

    added info

--- ReLu.py
+++ ReLu.py
@@ -38,8 +38,8 @@ def solve_NN(n, layer, input_range, invariance = 1):
     return end - start, x_out.value
 
 # Define parameter ranges
-n_range = range(2, 90, 3)
-layer_range = range(2, 15, 2)
+n_range = range(2, 50, 3)
+layer_range = range(2, 9, 2)
 input_range = range(1, 8, 2)
 
 # Initialize dictionary to store results
